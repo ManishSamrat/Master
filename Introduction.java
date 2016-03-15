27b6c7c1f4d7588a567f32f9a383cd3120d57a6a
Manish-Samrat <manishrajmax@gmail.com>
2016-03-15 11:09:56 +0530

    first commit

---
+++ Introduction.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.marvelous.controller;
+
+/**
+ * @author administrator
+ *
+ */
+public class Introduction {
+	public static void main(String args[])
+	{
+		System.out.println("Samrat The Emperor");
+	}
+
+}
